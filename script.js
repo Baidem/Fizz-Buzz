ce0d888ac06230e5dca03892ce460451c2bb6211
POUPAERT Vincent <baidem@gmail.com>
2022-01-26 18:22:31 +0100

    loop with setInterval

--- script.js
+++ script.js
@@ -1,23 +1,40 @@
-const play = document.getElementById("playButton");
-play.addEventListener("click", funFizzBuzz);
+const printResult = document.getElementById("text");
+const playButton = document.getElementById("playButton");
+const stopButton = document.getElementById("stopButton");
+playButton.addEventListener("click", funFizzBuzz);
+stopButton.addEventListener("click", stopRun);
 
+let index = 1;
+let pass = true;
 
-function funFizzBuzz() {
-    for (let index = 1; index < 101; index++) {
-        let tellMe = ""; // save
-        if (index % 3 == 0) { // si modulo de 3, Fizz est save.
-            tellMe = "Fizz";
-        }
-        if (index % 5 == 0) { // si modulo de 5, Buzz est save, si un Fizz est déja save, il est concervé.
-            tellMe = tellMe+"Buzz";
-        }
-        if (tellMe == "") { // si ni Fizz ni Buzz de save, l'index est save.
-            tellMe = index;
-        }
-        // on décharge la save en la print.
-                // un petit délais entre chaque print est souhaité, mais la fonction setTimeout ne fonctionne pas comme voulue. Des problèmes de bind à résoudre.
-        setTimeout(() => {document.getElementById("text").innerHTML = document.getElementById("text").innerHTML+"["+tellMe+"] "}, 500);
+function funFizzBuzz() { /* La fonction est appelée avec le bouton -Start to play-
+    La fonction est cadencée en boucle par interval de temps avec la méthode setInterval
+    La variable cursor est défini par : cursor = index + 100. Pour déterminer des session de 100 unités.
+        Chaque boucle test la variable index
+        Si est modulo de 3 : "Fizz" est save dans la variable tellMe
+        Si est modulo de 5 : tellMe + "Buzz" est save dans la variable tellMe
+        Si index == cursor : c'est la fin session, setInterval est mise en pause avec clearInterval
+    On peut aussi mettre la pause en le bouton -Stop- qui appelle la fonction stopRun()
+    On relance la boucle avec le bouton  -Start to play- */
+    if (pass == true) { // variable pass pour debugger playButton qui, quand on le spam, réduit l'interval et inactive le clearInterval
+        pass = false;
+        let cursor = index + 100;
+        intervalId = setInterval(function(){
+            let tellMe = "";
+            if(index % 3 == 0){tellMe = "Fizz";}
+            if(index % 5 == 0){tellMe = tellMe + "Buzz";}
+            if(tellMe == ""){tellMe = index;}
+            printResult.textContent = printResult.textContent + "[" + tellMe + "] ";
+            index += 1;
+            if (index == cursor) {
+                clearInterval(intervalId);
+                pass = true;
+            }
+            }, 500);    
     }
 }
 
-// la suite du projet et de print la suite par paquets de 100 unités à chaques préssions du bouton. Un changement de message sur le bouton est souhaité.
+function stopRun(){
+    clearInterval(intervalId);
+    pass = true;
+}
